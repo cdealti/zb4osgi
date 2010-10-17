f3e7fbd47e5833abe01efab53ed49ff991907066
stefano.lenzi <stefano.lenzi@a5b69b06-b4b2-4dab-a1bb-7adf784d9d5f>
2010-10-17 10:37:30 +0000

    Added a stub device represeing an OnOffSwitch for testing the binding in the ZigBee Tester ( refs 76 ) Added proper credits to sources Minor code polish

--- zigbee.basedriver.stub/src/main/java/it/cnr/isti/basedriver/stub/api/impl/OnOffOutputDevice.java
+++ zigbee.basedriver.stub/src/main/java/it/cnr/isti/basedriver/stub/api/impl/OnOffOutputDevice.java
@@ -28,25 +28,28 @@ import it.cnr.isti.zigbee.api.ZigBeeNode;
 /**
  * @author <a href="mailto:stefano.lenzi@isti.cnr.it">Stefano "Kismet" Lenzi</a>
  * @version $LastChangedRevision$ ($LastChangedDate$)
- * @since 0.1.0
- *
+ * @since 0.2.0 - Revision 118
+ * 
  */
-public class FloodDetectorDevice extends StubZigbeeDeviceBase {
+public class OnOffOutputDevice extends StubZigbeeDeviceBase {
 
-	public FloodDetectorDevice(int endpoint, final ZigBeeNode node){
+    public OnOffOutputDevice(int endpoint, final ZigBeeNode node){
 		super(
-				0x0509, 0, endpoint, 0x0104, 
+			0x0002, 0, endpoint, 0x0104,  
 			new int[]{
 					0x0000, //Basic
+					0x0001, //Power Configuration
+					0x0002, //Device Temperature Configuration						
 					0x0003, //Identify
 					0x0004, //Scenes
 					0x0005, //Group
-					(short) 0xFC0A, //FloodMeasurement
-					0x0402  //TemperatureMeasurement
-			}, 
-			new int[]{}, node
-		);		
+					0x0006, //OnOff
+					0x0009, //Alarms
+			}, new int[]{
+			}, node
+		);
 	}
+	
 	@Override
 	public Cluster stubInvoke(Cluster cluster) throws ZigBeeBasedriverException {
 		// TODO Auto-generated method stub
--- zigbee.basedriver.stub/src/main/java/it/cnr/isti/basedriver/stub/api/impl/OnOffSwitchDevice.java
+++ zigbee.basedriver.stub/src/main/java/it/cnr/isti/basedriver/stub/api/impl/OnOffSwitchDevice.java
@@ -28,25 +28,30 @@ import it.cnr.isti.zigbee.api.ZigBeeNode;
 /**
  * @author <a href="mailto:stefano.lenzi@isti.cnr.it">Stefano "Kismet" Lenzi</a>
  * @version $LastChangedRevision$ ($LastChangedDate$)
- * @since 0.1.0
- *
+ * @since 0.2.0 - Revision 118
+ * 
  */
-public class FloodDetectorDevice extends StubZigbeeDeviceBase {
+public class OnOffSwitchDevice extends StubZigbeeDeviceBase {
 
-	public FloodDetectorDevice(int endpoint, final ZigBeeNode node){
+	public OnOffSwitchDevice(int endpoint, final ZigBeeNode node){
 		super(
-				0x0509, 0, endpoint, 0x0104, 
+			0x0000, 0, endpoint, 0x0104,  
 			new int[]{
-					0x0000, //Basic
-					0x0003, //Identify
-					0x0004, //Scenes
-					0x0005, //Group
-					(short) 0xFC0A, //FloodMeasurement
-					0x0402  //TemperatureMeasurement
-			}, 
-			new int[]{}, node
-		);		
+                0x0000, //Basic - HA Mandatory Common Cluster
+                0x0003, //Identify - HA Mandatory Common Cluster
+                0x0001, //Power Configuration - HA Optional Common Cluster
+                0x0002, //Device Temperature Configuration - HA Optional Common Cluster
+                0x0009, //Alarms - HA Optional Common Cluster
+                0x0007, //OnOff Switch Configuration - HA On/Off Switch Device Mandatory Cluster
+			}, new int[]{
+                0x0006, //OnOff - HA On/Off Switch Device Mandatory Cluster
+                0x0004, //Group - HA On/Off Switch Device Mandatory Cluster
+                0x0005, //Scenes - HA On/Off Switch Device Mandatory Cluster
+                0x0003, //Identify - HA On/Off Switch Device Mandatory Cluster
+			}, node
+		);
 	}
+	
 	@Override
 	public Cluster stubInvoke(Cluster cluster) throws ZigBeeBasedriverException {
 		// TODO Auto-generated method stub
@@ -59,4 +64,8 @@ public class FloodDetectorDevice extends StubZigbeeDeviceBase {
 		
 	}
 
+	public ZigBeeNode getPhysicalNode() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }

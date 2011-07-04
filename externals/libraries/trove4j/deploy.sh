f4601e4c0678fe15f5327dcb87b5d77118fb0b4c
stefano.lenzi <stefano.lenzi@a5b69b06-b4b2-4dab-a1bb-7adf784d9d5f>
2011-07-04 22:09:44 +0000

    Configuration used for releasing net.sf.trove4j 2.0.4

--- externals/libraries/trove4j/deploy.sh
+++ externals/libraries/trove4j/deploy.sh
@@ -2,8 +2,10 @@
 ARTIFACT=trove
 REPO_ID="-DrepositoryId=release.aaloa.org"
 REPO_URL="-Durl=http://nexus.aaloa.org/content/repositories/releases"
-OPT="$REPO_ID $REPO_URL"
+POM="-DpomFile=pom.xml"
+FILE="-Dfile=${ARTIFACT}.jar"
+SRC="-Dsources=${ARTIFACT}-sources.jar"
+DOC="-Djavadoc=${ARTIFACT}-javadocs.jar"
+OPT="$REPO_ID $REPO_URL $POM $FILE $SRC $DOC"
 
-mvn deploy:deploy-file $OPT -DpomFile=pom.xml -Dfile=${ARTIFACT}.jar 
-mvn deploy:deploy-file $OPT -DpomFile=pom.xml -Dfile=${ARTIFACT}-sources.jar -Dclassifier=sources
-mvn deploy:deploy-file $OPT -DpomFile=pom.xml -Dfile=${ARTIFACT}-javadocs.jar -Dclassifier=javadocs
+mvn org.apache.maven.plugins:maven-deploy-plugin:2.6:deploy-file $OPT
